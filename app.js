fc3f9a0f75ad6fac4c1203e4439ee281058f6e5f
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-10-21 20:20:11 -0700

    [app.js] add filter method to query in the search method

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// set an empty array where we will insert all the cities and state object coming from the endpoint
+const cities = []
+
+// get the json data from the endpoint using fetch
+fetch(endpoint)
+// if you console log the data you will see different kinds of file, but we need to go in the json to access the cities and state.
+    // fetching the data in the json file. that is why we have "data.json()"
+    .then(data => data.json())
+
+    // now that we have our json data, we need to transfer the json data into our cities array.
+    .then(result => cities.push(...result))
+    
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        //  here we need to figure our if the city or state matches what was search
+        // in the regex "g" means global, it will search in the whole element. "i" means insensitive, it can be search either if the character is uppercase or lowercase.
+        const regex = new RegExp(wordToMatch, 'gi')
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
